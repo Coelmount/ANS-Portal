dbdf1916b834ad51be89bfc6ecea5f804d908064
Voronenkov Artem <a.voronenkov@sharksw.com>
2020-03-06 12:37:29 +0200

    PhoneNumber input fixed

--- src/components/PhoneNumberInput/index.js
+++ src/components/PhoneNumberInput/index.js
@@ -2,13 +2,36 @@ import React, { useState, useEffect } from 'react'
 
 import TextField from '@material-ui/core/TextField'
 import InputMask from 'react-input-mask'
+import Box from '@material-ui/core/Box'
 
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative'
+  },
+  icon: {
+    position: 'absolute',
+    zIndex: '1',
+    left: '24px',
+    height: '20px',
+    width: '20px',
+    textAlign: 'center'
+  },
   input: {
-    width: '154px',
-    height: '50px'
+    width: '392px',
+    '& .MuiInputLabel-outlined': {
+      left: '49px'
+    },
+    '& .MuiInputLabel-shrink': {
+      transform: 'translate(-35px, -11px) scale(0.75)',
+      color: '#666666'
+    },
+    '& .MuiOutlinedInput-input': {
+      padding: '18.5px 14px 18.5px 49px'
+    }
   }
 }))
 
@@ -37,23 +60,26 @@ const PhoneNumberInput = props => {
   }
 
   return (
-    <InputMask
-      {...props}
-      onChange={e => {
-        props.onChange(inputValue)
-        handleInputChange(e)
-      }}
-      mask='+29 999 999 999'
-      value={valueArr}
-    >
-      {() => (
-        <TextField
-          placeholder='+2_ ___ ___ ___'
-          className={classes.input}
-          variant='outlined'
-        />
-      )}
-    </InputMask>
+    <Box className={classes.root}>
+      {!!props.icon && <Box className={classes.icon}>{props.icon}</Box>}
+      <InputMask
+        {...props}
+        onChange={e => {
+          props.onChange(inputValue)
+          handleInputChange(e)
+        }}
+        mask='+29 999 999 999'
+        value={valueArr}
+      >
+        {() => (
+          <TextField
+            placeholder='+2_ ___ ___ ___'
+            className={classes.input}
+            variant='outlined'
+          />
+        )}
+      </InputMask>
+    </Box>
   )
 }
 
