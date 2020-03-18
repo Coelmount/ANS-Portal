9a69ab0fe20c7d496aa25379ea5453bbc7ab3ebc
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2020-03-18 15:36:35 +0200

    admin customer sore?

---
+++ src/stores/EditDeleteAdministrator.js
@@ -0,0 +1,75 @@
+import { createContext } from 'react'
+import { decorate, observable, action } from 'mobx'
+
+import axios from 'utils/axios'
+
+export class EditDeleteAdminStore {
+  isLoadingData = true
+
+  updatedAdmin = {
+    firstName: '',
+    lastName: '',
+    language: ''
+  }
+  isDeletingAdmin = false
+  sentAdmin = {}
+  updateAdminInfo = (valueKey, value) => {
+    for (let key in this.updatedAdmin) {
+      if (valueKey === key) {
+        this.updatedAdmin[key] = value
+        this.sentAdmin[key] = value
+      }
+    }
+  }
+
+  getAdminInfo = ({ id, userId }) => {
+    this.isLoadingData = true
+
+    axios.get(`tenants/${id}/admins/${userId}`).then(res => {
+      if (res.status === 200) {
+        this.updateAdminInfo('firstName', res.data.firstName)
+        this.updateAdminInfo('lastName', res.data.lastName)
+        this.updateAdminInfo('language', res.data.language)
+        this.isLoadingData = false
+      } else {
+        console.log(res, 'error')
+      }
+    })
+  }
+  updateCustomerAdmin = ({ id, closeModal, userId, getUsers }) => {
+    this.isLoadingData = true
+    axios.put(`/tenants/${id}/admins/${userId}`, this.sentAdmin).then(res => {
+      if (res.status === 200) {
+        console.log('updated')
+        this.isLoadingData = false
+        closeModal()
+        getUsers(id)
+      } else {
+        console.log(res, 'error')
+      }
+    })
+  }
+  deleteAdmin = ({ id, closeModal, userId, getUsers }) => {
+    this.isDeletingAdmin = true
+    axios.delete(`/tenants/${id}/admins/${userId}`).then(res => {
+      if (res.status === 200) {
+        getUsers(id)
+        closeModal()
+        this.isDeletingAdmin = false
+      } else {
+        console.log(res, 'error')
+      }
+    })
+  }
+}
+decorate(EditDeleteAdminStore, {
+  isLoadingData: observable,
+  isDeletingAdmin: observable,
+  updatedAdmin: observable,
+  updateAdminInfo: action,
+  getAdminInfo: action,
+  updateCustomerAdmin: action,
+  deleteAdmin: action
+})
+
+export default createContext(new EditDeleteAdminStore())
