94b6504f6e39879125001d3a0f6ecfb0c33d38ba
Voronenkov Artem <a.voronenkov@sharksw.com>
2020-03-31 09:40:01 +0200

    pagination and deassign button added

--- src/ui/Customer/AccessNumbersItem/styles.js
+++ src/ui/Customer/AccessNumbersItem/styles.js
@@ -233,6 +233,7 @@ const useStyles = makeStyles(theme => ({
   perPageWrap: {
     display: 'flex',
     alignItems: 'center',
+    width: 75,
     '& .MuiInput-underline:before': {
       display: 'none'
     },
@@ -256,6 +257,7 @@ const useStyles = makeStyles(theme => ({
     '& div': {
       paddingLeft: 16,
       paddingTop: 8,
+      paddingRight: '28px !important',
       width: 66,
       fontSize: 14
     },
@@ -315,6 +317,52 @@ const useStyles = makeStyles(theme => ({
     background: 'white',
     borderRadius: 100,
     boxShadow: '0px 4px 4px rgba(204, 204, 204, 0.25)'
+  },
+  titleWrap: {
+    display: 'flex',
+    height: 91,
+    alignItems: 'center'
+  },
+  title: {
+    flex: '1 1 100%',
+    fontFamily: 'MTN',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    fontSize: 24,
+    letterSpacing: '0.02em',
+    color: theme.palette.black
+  },
+  addCustomerWrap: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  addIconWrap: {
+    width: 30,
+    height: 30,
+    background: theme.palette.primary.main,
+    borderRadius: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 8,
+    '&:hover': {
+      cursor: 'pointer'
+    },
+    color: theme.palette.black,
+    '& > img': {
+      width: 14,
+      height: 16
+    }
+  },
+  addIcon: {
+    color: theme.palette.black
+  },
+  addCustomerTitle: {
+    whiteSpace: 'nowrap',
+    fontSize: 14,
+    fontFamily: 'MTN',
+    fontStyle: 'normal',
+    fontWeight: 500
   }
 }))
 
