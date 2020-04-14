d9bece89ec5fc6ca023f16ec22bd27ffa9765023
artemvoronenkovSw <a.voronenkov@sharksw.com>
2020-04-14 19:27:51 +0300

    Title padding changed

--- src/components/TitleBlock/styles.js
+++ src/components/TitleBlock/styles.js
@@ -1,9 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   titleWrap: {
     display: 'flex',
-    height: 91,
+    height: 80,
     alignItems: 'center'
   },
   title: {
