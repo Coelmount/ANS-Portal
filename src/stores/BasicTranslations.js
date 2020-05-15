73110cbf664abfb5e676f73c254f3ca3522230b3
artemvoronenkovSw <a.voronenkov@sharksw.com>
2020-05-15 14:56:31 +0300

    SUBACCOUNT-Basic-Translations access number column updated

--- src/stores/BasicTranslations.js
+++ src/stores/BasicTranslations.js
@@ -48,7 +48,10 @@ export class BasicTranslations {
             checked: false,
             hover: false,
             enabled: true,
-            accessCountry: getCountryNameFromNumber(item.access_number),
+            accessCountry:
+              (item.access_number &&
+                getCountryNameFromNumber(item.access_number)) ||
+              'none',
             destinationCountry: getCountryNameFromNumber(
               item.destination_number
             ),
