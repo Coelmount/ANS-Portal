71f7880a7dd61bda204129238c8856c256281bd8
artemvoronenkovSw <a.voronenkov@sharksw.com>
2020-06-17 11:42:24 +0300

    Event tooltip disabled, popover small fix, refactoring

--- src/components/WeekSchedule/index.js
+++ src/components/WeekSchedule/index.js
@@ -7,16 +7,15 @@ import globalize from 'globalize'
 import { Calendar, Views, momentLocalizer } from 'react-big-calendar'
 import localizer from 'react-big-calendar/lib/localizers/globalize'
 
-import CustomContainer from 'components/CustomContainer'
-import ExtendedTitleBlock from 'components/ExtendedTitleBlock'
-import CustomBreadcrumbs from 'components/CustomBreadcrumbs'
-
 import Paper from '@material-ui/core/Paper'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import AddOutlinedIcon from '@material-ui/icons/AddOutlined'
 
 import WeekSchedulesStore from 'stores/WeekSchedules'
+import CustomContainer from 'components/CustomContainer'
+import ExtendedTitleBlock from 'components/ExtendedTitleBlock'
+import CustomBreadcrumbs from 'components/CustomBreadcrumbs'
 import Loading from 'components/Loading'
 import PopoverBlock from './components/PopoverBlock/index.jsx'
 import AddPeriodModal from './components/AddPeriodModal'
@@ -25,12 +24,11 @@ import DeletePeriodsModal from './components/DeletePeriodsModal'
 import { WEEK_DAYS_ARR } from 'utils/schedules/weekDaysArr'
 import transformTime from 'utils/schedules/transformTime'
 
-import * as dates from './dates'
-import useStyles from './styles'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 import blueEditIcon from 'source/images/svg/edit-blue.svg'
 import editIcon from 'source/images/svg/edit.svg'
 import deleteIcon from 'source/images/svg/delete-icon.svg'
+import useStyles from './styles'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
 const WeekSchedule = observer(({ t }) => {
   const classes = useStyles()
@@ -105,6 +103,7 @@ const WeekSchedule = observer(({ t }) => {
     setIsEditScheduleModalOpen(true)
     setIsSinglePeriodEditActive(true)
     findPeriodAndSetToEdit(currentPeriod)
+    setAnchorEl(null)
   }
 
   // POPOVER  DELETE
@@ -225,6 +224,7 @@ const WeekSchedule = observer(({ t }) => {
               components={{
                 event: EventComponent
               }}
+              tooltipAccessor={null}
               selectable
             />
             <PopoverBlock
