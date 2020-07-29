9b359f081c580cb52486915e6010bb66d207686c
Voronenkov Artem <artemvoronenkov@gmail.com>
2020-07-29 18:35:05 +0300

    List page view, logic updated, Add Modal view, logic, part of API integration done

--- src/ui/Subaccount/AnsInstances/TimeBasedRouting/index.js
+++ src/ui/Subaccount/AnsInstances/TimeBasedRouting/index.js
@@ -1,63 +1,64 @@
 import React, { useState, useEffect } from 'react'
-import { observer } from 'mobx-react'
+import { observer, useLocalStore } from 'mobx-react-lite'
 import { withNamespaces } from 'react-i18next'
-import { useParams, useHistory } from 'react-router-dom'
-import MaterialLink from '@material-ui/core/Link'
+import { useParams, useHistory, Link } from 'react-router-dom'
 
+import AddIcon from '@material-ui/icons/Add'
+import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined'
 import Paper from '@material-ui/core/Paper'
 import IconButton from '@material-ui/core/IconButton'
 
-//import Switch from 'components/Switch'
+import TimeBaseRoutingStore from 'stores/TimeBasedRouting'
 import Loading from 'components/Loading'
 import TitleBlock from 'components/TitleBlock'
 import CustomContainer from 'components/CustomContainer'
 import CustomBreadcrumbs from 'components/CustomBreadcrumbs'
 import CustomTable from 'components/CustomTable'
-// import AddIVR from './AddIVR'
-//import DisableEnableIVR from 'components/DeleteModal'
 import DeleteModal from 'components/DeleteModal'
-
-import TimeBaseRoutingStore from 'stores/TimeBaseRouting'
-
-import AddIcon from '@material-ui/icons/Add'
-import CloseOutlinedIcon from '@material-ui/icons/CloseOutlined'
+import AddModal from './components/AddModal'
 
 import useStyles from './styles'
 
-const TimeBaseRouting = props => {
-  const { t } = props
+const addModalId = 1
+const deleteModalId = 2
+
+const TimeBasedRouting = ({ t }) => {
   const classes = useStyles()
-  const match = useParams()
   const history = useHistory()
+  const { customerId, groupId } = useParams()
+
   const {
-    timeBaseRouting,
+    timeBaseRoutes,
     isLoadingTBR,
-    getTimeBaseRouting
+    getTimeBasedRoutes
   } = TimeBaseRoutingStore
-  // const [showAddIVR, setShowAddIVR] = useState(false)
-  // const [ivrForSwitchStatus, setIVRForSwitchStatus] = useState(null)
-  // const [ivrForDelete, setIVRForDelete] = useState(null)
+
+  const modalsStore = useLocalStore(() => ({
+    openedId: null,
+    open(modalId) {
+      this.openedId = modalId
+    },
+    close() {
+      this.openedId = null
+      getRequest()
+    }
+  }))
+
+  const getRequest = () => {
+    const payload = {
+      customerId,
+      groupId
+    }
+    getTimeBasedRoutes(payload)
+  }
 
   useEffect(() => {
-    getTimeBaseRouting(match.customerId, match.groupId)
+    getRequest()
   }, [])
 
-  // const handleDelete = () => {
-  //   deleteIVR(
-  //     match.customerId,
-  //     match.groupId,
-  //     ivrForDelete.serviceUserId,
-  //     handleClose
-  //   )
-  // }
-
-  // const handleClose = () => {
-  //   setShowAddIVR(false)
-  //   setIVRForSwitchStatus(null)
-  //   setIVRForDelete(null)
-  //   getIVRs(match.customerId, match.groupId)
-  //   getCheckLicensesIVR(match.customerId, match.groupId)
-  // }
+  const handleAddButtonClick = () => {
+    modalsStore.open(addModalId)
+  }
 
   const titleData = {
     mainText: t('time_base_routing'),
@@ -68,22 +69,18 @@ const TimeBaseRouting = props => {
   const columns = [
     {
       id: 'name',
-      numeric: false,
       label: t('name'),
-      extraProps: {
-        scope: 'row'
-      },
       getCellData: row => (
-        <MaterialLink
+        <Link
           onClick={() =>
             history.push(
-              `/customers/${match.customerId}/subaccounts/${match.groupId}/ivr/${row.serviceUserId}`
+              `/customers/${customerId}/subaccounts/${groupId}/ivr/${row.serviceUserId}`
             )
           }
           className={classes.link}
         >
           {row.name}
-        </MaterialLink>
+        </Link>
       )
     },
     {
@@ -106,10 +103,6 @@ const TimeBaseRouting = props => {
     }
   ]
 
-  if (isLoadingTBR) {
-    return <Loading />
-  }
-
   return (
     <div className={classes.root}>
       <Paper className={classes.paper}>
@@ -118,34 +111,30 @@ const TimeBaseRouting = props => {
           <TitleBlock
             titleData={titleData}
             classes={classes}
-            //handleOpen={() => setShowAddIVR(true)}
+            handleOpen={handleAddButtonClick}
           />
         </CustomContainer>
-        <CustomTable
-          rows={[]}
-          columns={columns}
-          searchCriterias={['name']}
-          noAvailableDataMessage={t('no_tbs_available')}
-          tableId='tbs'
-        />
-        {/* {showAddIVR && <AddIVR open={showAddIVR} handleClose={handleClose} />}
-        {ivrForDelete && (
-          <DeleteModal
-            open={ivrForDelete}
-            handleClose={handleClose}
-            handleDelete={handleDelete}
-            isDeleting={isDeletingIVR}
-            action={t('to_delete')}
-            titleAction={t(`delete`)}
-            deleteSubject={t('ans_ivr_instance')}
-            deleteInfo={{
-              name: ivrForDelete.name
-            }}
+        {isLoadingTBR ? (
+          <Loading />
+        ) : (
+          <CustomTable
+            rows={timeBaseRoutes}
+            columns={columns}
+            searchCriterias={['name']}
+            noAvailableDataMessage={t('no_tbs_available')}
+            tableId='time_based_routing_list'
           />
-        )} */}
+        )}
       </Paper>
+
+      {modalsStore.openedId === addModalId && (
+        <AddModal
+          open={modalsStore.openedId === addModalId}
+          handleClose={modalsStore.close}
+        />
+      )}
     </div>
   )
 }
 
-export default withNamespaces()(observer(TimeBaseRouting))
+export default withNamespaces()(observer(TimeBasedRouting))
--- src/ui/Subaccount/AnsInstances/TimeBasedRouting/styles.js
+++ src/ui/Subaccount/AnsInstances/TimeBasedRouting/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core/styles/'
+import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,63 +10,246 @@ const useStyles = makeStyles(theme => ({
     boxShadow: 'none'
   },
   link: {
-    cursor: 'pointer'
-  },
-  rootEditSchedule: {
-    '& .MuiDialog-paperWidthSm': {
-      width: '650px'
-    },
-    '& .MuiDialog-paperScrollPaper': {
-      height: 540
+    color: theme.palette.secondary.main,
+    textDecoration: 'none',
+    '&:focus': {
+      color: '#598597'
     }
   },
-  title: {
-    '& > *': {
-      fontFamily: 'MTN',
-      fontWeight: 700,
-      fontSize: '24px',
-      color: theme.palette.black
-    },
-    height: '90px',
-    padding: '25px 44px'
-  },
-  closeButton: {
-    position: 'absolute',
-    right: 10,
-    top: 20,
-    '& > span > svg': {
-      width: 25,
-      height: 25
-    }
-  },
-  dialogContent: {
+  addCustomerWrap: {
     display: 'flex',
-    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  updateWrap: {
+    display: 'flex',
     alignItems: 'center',
-    flexDirection: 'column'
+    marginRight: 30
   },
-  dialogActions: {
-    height: '110px',
-    justifyContent: 'space-between'
+  deleteBlockWrap: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: 50
   },
-  cancelButton: {
-    width: '160px',
-    width: '160px',
-    marginLeft: '32px',
+  addIconWrap: {
+    width: 30,
+    height: 30,
+    background: theme.palette.primary.main,
+    borderRadius: 100,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 8,
+    '&:hover': {
+      cursor: 'pointer'
+    },
     color: theme.palette.black
   },
-  assignButton: {
-    width: '160px',
-    marginRight: '32px'
+  activeIcon: {
+    background: theme.palette.primary.main
   },
-  select: {
-    width: 254
+  disabledIcon: {
+    backgroundColor: theme.palette.lightGrey
   },
-  editSchedulesText: {
-    marginBottom: 10,
-    fontSize: 16,
+  addIcon: {
+    color: theme.palette.black
+  },
+  mainIconWrap: {
+    background: theme.palette.primary.main,
+    width: 30,
+    height: 30,
+    padding: 0,
+    marginRight: 8,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.main
+    }
+  },
+  disabledButton: {
+    backgroundColor: theme.palette.lightGrey,
+    '&:hover': {
+      backgroundColor: theme.palette.lightGrey,
+      cursor: 'auto'
+    }
+  },
+  disabledText: {
+    opacity: 0.5
+  },
+  deleteIcon: {
+    width: 14,
+    height: 16
+  },
+  numbersWrap: {
+    display: 'flex'
+  },
+  rightArrowIcon: {
+    marginRight: 31,
+    marginLeft: 31
+  },
+  numbersAmount: {
+    marginLeft: 10
+  },
+  numbersTitle: {
+    fontSize: 14,
+    fontWeight: 600
+  },
+  toolbarButtonsBlockWrap: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    width: '100%',
+    marginRight: 40,
+    marginLeft: 40
+  },
+  checkbox: {
+    boxShadow: '0px 2px 4px rgba(204, 204, 204, 0.25)',
+    height: '18px',
+    marginLeft: '2px',
+    '& .MuiSvgIcon-root': {
+      color: theme.palette.primary.main
+    },
+    '& .MuiIconButton-root': {
+      padding: 0
+    }
+  },
+  headCheckbox: {
+    marginLeft: 5,
+    marginBottom: 2
+  },
+  checkboxCell: {
+    width: 85,
+    paddingLeft: `20px !important`,
+    '& > div': {
+      marginLeft: 4
+    }
+  },
+  indexHoverCheckbox: {
+    marginLeft: 3
+  },
+  accessNumberText: {
+    color: theme.palette.active.blue,
+    fontSize: 14,
+    letterSpacing: '0.05em'
+  },
+  destinationNumberText: {
+    fontWeight: 600,
+    fontSize: 14,
+    letterSpacing: '0.05em'
+  },
+  accessNumberCell: {
+    paddingLeft: '52px !important'
+  },
+  accessHeadCell: {
+    width: 268,
+    paddingLeft: 52,
+    '& span': {
+      fontSize: 16,
+      fontWeight: 600
+    }
+  },
+  switchCell: {
+    paddingLeft: '46px !important'
+  },
+  switchBase: {
+    color: 'white',
+    '&$checked': {
+      color: 'white'
+    },
+    '&$checked + $track': {
+      color: 'white',
+      background: theme.palette.primary.main,
+      opacity: 1
+    }
+  },
+  track: {
+    background: 'grey'
+  },
+  checked: {},
+  upArrowIcon: {
+    position: 'absolute',
+    right: -14,
+    top: -3,
+    color: theme.palette.active.blue
+  },
+  downArrowIcon: {
+    position: 'absolute',
+    right: -14,
+    top: 5,
+    color: theme.palette.active.blue
+  },
+  addCustomerTitle: {
+    marginRight: 10,
+    fontSize: 14,
+    fontStyle: 'normal',
     fontFamily: 'MTN',
-    fontWeight: 500
+    fontWeight: 500,
+    whiteSpace: 'nowrap'
+  },
+  addTitleWrap: {
+    position: 'relative'
+  },
+  addPopoverWrap: {
+    width: 268,
+    height: 101
+  },
+  addPopoverItem: {
+    height: '50%',
+    paddingLeft: 30,
+    '&:nth-child(1)': {
+      borderBottom: `1px solid ${theme.palette.active.main}`
+    },
+    '& > a': {
+      textDecoration: 'none'
+    }
+  },
+  addPopoverItemText: {
+    color: theme.palette.black
+  },
+  deleteCustomerIcon: {
+    width: 20,
+    height: 20,
+    color: theme.palette.black,
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  },
+  deleteCell: {
+    paddingRight: '38px !important',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  toolbarButton: {
+    width: 30,
+    height: 30,
+    minWidth: 30,
+    marginRight: 8,
+    borderRadius: 100,
+    padding: '6px 15px'
+  },
+  updateIcon: {
+    width: 18,
+    height: 18
+  },
+  titleWrap: {
+    display: 'flex'
+  },
+  titleNotification: {
+    padding: 23,
+    marginRight: 9,
+    fontSize: 14,
+    background: 'white',
+    border: `0.5px solid ${theme.palette.silver}`,
+    boxShadow:
+      '0px 4px 8px rgba(204, 204, 204, 0.25), inset 0px 2px 4px rgba(204, 204, 204, 0.25)',
+    borderRadius: 3
+  },
+  notificationIcon: {
+    marginRight: 5
+  },
+  nameCellTitle: {
+    color: theme.palette.active.blue,
+    fontSize: 14,
+    cursor: 'pointer'
+  },
+  deleteName: {
+    fontWeight: 'bold'
   }
 }))
 
