0ff4831fa5203c95d2c7229cb831f412f768775d
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2020-08-20 10:48:03 +0300

    logic for close snackbar

--- src/components/Snackbar/index.js
+++ src/components/Snackbar/index.js
@@ -3,6 +3,7 @@ import { withSnackbar } from 'notistack'
 import { inject, observer } from 'mobx-react'
 import { autorun } from 'mobx'
 import SneckbarStore from 'stores/Snackbar'
+import NotificationContent from './Snackbar'
 
 class Notifier extends Component {
   displayed = []
@@ -19,7 +20,18 @@ class Notifier extends Component {
         // Do nothing if snackbar is already displayed
         if (this.displayed.includes(notification.key)) return
         // Display snackbar using notistack
-        this.props.enqueueSnackbar(notification.message, notification.options)
+        this.props.enqueueSnackbar(notification.message, {
+          ...notification.options,
+          //classes: props.classes,
+          content: (key, message) => (
+            <NotificationContent
+              id={key}
+              message={message}
+              options={notification.options}
+            />
+          )
+        })
+        //this.props.enqueueSnackbar(notification.message, notification.options)
         // Keep track of snackbars that we've displayed
         this.storeDisplayed(notification.key)
         // Dispatch action to remove snackbar from mobx store
