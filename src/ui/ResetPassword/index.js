6ceaf938ec89b460b58764e0abfa8848cf62bd7d
Voronenkov Artem <artemvoronenkov@gmail.com>
2020-09-11 18:45:42 +0300

    lint warning fixed

--- src/ui/ResetPassword/index.js
+++ src/ui/ResetPassword/index.js
@@ -10,7 +10,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
-import MaterialLink from '@material-ui/core/Link'
+// import MaterialLink from '@material-ui/core/Link'
 
 import PermIdentityOutlined from '@material-ui/icons/PermIdentityOutlined'
 // import LockOutlined from '@material-ui/icons/LockOutlined'
