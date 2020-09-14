62bd417682a79f40c662ebcee6952b27c063204d
Voronenkov Artem <artemvoronenkov@gmail.com>
2020-09-14 14:16:35 +0300

    translation added to getHelperText util

--- src/utils/getHelperText.js
+++ src/utils/getHelperText.js
@@ -1,16 +1,20 @@
 // Returns provided helper text or generated one: Remove ":" symbol from title
 // then replace spaces with underscore symbol
 
-const getHelperText = (title, providedHelperText) => {
+import i18n from 'i18next'
+
+const getHelperText = (title = '', providedHelperText) => {
   const generatedHelperText = title
     .replace(':', '')
     .split(' ')
     .join('_')
     .toLowerCase()
 
-  return `helper_text_${
-    providedHelperText ? providedHelperText : generatedHelperText
-  }`
+  return i18n.t(
+    `helper_text_${
+      providedHelperText ? providedHelperText : generatedHelperText
+    }`
+  )
 }
 
 export default getHelperText
