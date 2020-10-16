d1261c93a2bdc24ebb0ee14479a22a0cf626963e
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2020-10-16 13:20:06 +0300

    fixed bug with reserved numbers

--- src/ui/Customer/AccessNumbersItem/AddNumbers/index.js
+++ src/ui/Customer/AccessNumbersItem/AddNumbers/index.js
@@ -32,8 +32,8 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-const CreateCustomer = props => {
-  const { open, handleClose, step, changeStep } = props
+const AddNumbers = props => {
+  const { open, handleClose, step, changeStep, availableSlots } = props
   const {
     getAvailableNumbers,
     availableNumbers,
@@ -218,6 +218,7 @@ const CreateCustomer = props => {
           addedNumbersSub={addedNumbersSub}
           changeHover={changeHover}
           changeHoverAddedNumbers={changeHoverAddedNumbers}
+          maxRangeSize={maxRangeSize}
         />
       ) : (
         <StepsNotFoundNumbers
@@ -306,6 +307,7 @@ const Steps = props => {
           selectAll={props.selectAll}
           postAddNumbersToCustomer={props.postAddNumbersToCustomer}
           changeHover={props.changeHover}
+          maxRangeSize={props.maxRangeSize}
         />
       )
     case 3:
@@ -349,4 +351,4 @@ const Steps = props => {
   }
 }
 
-export default withNamespaces()(observer(CreateCustomer))
+export default withNamespaces()(observer(AddNumbers))
