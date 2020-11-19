037b6fd1733bc706c4ed12b1f56d3490a8b19a6f
Viktor Smakhtin <viktor.smakhtin@sharksw.com>
2020-11-19 11:28:22 +0200

    added logic for deassign phone numbers from the group on the subaccount level

--- src/ui/Subaccount/PhoneNumbers/index.js
+++ src/ui/Subaccount/PhoneNumbers/index.js
@@ -3,13 +3,20 @@ import { observer } from 'mobx-react-lite'
 import { withNamespaces } from 'react-i18next'
 import { useParams, Link } from 'react-router-dom'
 import { useDebounce } from 'use-debounce'
+import classnames from 'classnames'
 
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import Box from '@material-ui/core/Box'
 import Chip from '@material-ui/core/Chip'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+import { withStyles } from '@material-ui/core/styles'
 
 import PhoneNumbersStore from 'stores/PhoneNumbers'
+import AssignedNumbersStore from 'stores/AssignedNumbers'
 import TitleBlock from 'components/TitleBlock'
 import CustomTable, {
   DEFAULT_ROWS_PER_PAGE
@@ -18,9 +25,17 @@ import CustomContainer from 'components/CustomContainer'
 import CustomBreadcrumbs from 'components/CustomBreadcrumbs'
 import FiltersModal from './components/FiltersModal'
 import Loading from 'components/Loading'
+import DeleteModal from 'components/DeleteModal'
 
 import filtersIcon from 'source/images/svg/filters.svg'
+import deassignIcon from 'source/images/svg/deassign.svg'
 import useStyles from './styles'
+
+const StyledTooltip = withStyles({
+  tooltip: {
+    textAlign: 'center'
+  }
+})(Tooltip)
 
 const PhoneNumbers = observer(({ t }) => {
   const storageRowsPerPage = localStorage.rowsPerPageScheme
@@ -41,6 +56,11 @@ const PhoneNumbers = observer(({ t }) => {
   const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false)
   const debouncedNumberLike = useDebounce(numberLike, 1000)[0]
 
+  const [isDeassignModalOpen, setIsDeassignModalOpen] = useState(false)
+  const [deassignMessageBlock, setDeassignMessageBlock] = useState(null)
+  const [deassignMessage, setDeassignMessage] = useState('')
+  const [deassignSubject, setDeassignSubject] = useState('')
+
   const {
     transformedPhoneNumbers,
     setPhoneNumbers,
@@ -51,6 +71,13 @@ const PhoneNumbers = observer(({ t }) => {
     filterValues,
     deleteSearchParam
   } = PhoneNumbersStore
+
+  const {
+    isDeassigningNumber,
+    showErrorNotification,
+    setNumbersToDeassign,
+    deassignNumbersSubLvl
+  } = AssignedNumbersStore
 
   // Filter or search params ? TRUE : FALSE
   const isSearchParamsActive =
@@ -143,6 +170,101 @@ const PhoneNumbers = observer(({ t }) => {
     deleteSearchParam(field)
   }
 
+  const handleDeassignAll = () => {
+    if (numbers.filter(el => el.status !== 'used').every(el => el.checked)) {
+      const newNumbers = numbers.map(el => {
+        if (el.status === 'used') {
+          return el
+        } else {
+          return { ...el, checked: false }
+        }
+      })
+      setNumbers(newNumbers)
+    } else {
+      const newNumbers = numbers.map(el => {
+        if (el.status === 'used') {
+          return el
+        } else {
+          return { ...el, checked: true }
+        }
+      })
+      setNumbers(newNumbers)
+    }
+  }
+
+  const singleDeassignSelect = row => {
+    const newNumbers = numbers.map(el => {
+      if (el.id === row.id) {
+        return { ...el, checked: !row.checked }
+      } else {
+        return el
+      }
+    })
+    setNumbers(newNumbers)
+  }
+
+  const createDeassignMessage = numbers => {
+    const amountOfNumbers = numbers.length
+    const deassignText =
+      amountOfNumbers > 1
+        ? t('phone_numbers').toLowerCase()
+        : t('phone_number').toLowerCase()
+    setDeassignSubject(deassignText)
+    const totalMessage = `${amountOfNumbers} ${deassignText}:`
+    setDeassignMessage(totalMessage)
+  }
+
+  const createDeassignMessageBlock = numbers => {
+    const numbersArr = numbers.map(item => item.phoneNumber)
+    const splitedNumbersStr = numbersArr.join(', ')
+    const totalMessage = `${splitedNumbersStr} ?`
+    const deassignMessageBlock = (
+      <Box>
+        <Typography className={classes.boldDeassignText}>
+          {totalMessage}
+        </Typography>
+        <Typography>{t('deassign_message_end')}</Typography>
+      </Box>
+    )
+    setDeassignMessageBlock(deassignMessageBlock)
+  }
+
+  const handleDeassignButtonClick = () => {
+    if (numbers.filter(el => el.status !== 'used').some(el => el.checked)) {
+      const selectedNumbers = numbers.filter(el => el.checked)
+      setNumbersToDeassign(selectedNumbers)
+      createDeassignMessage(selectedNumbers)
+      createDeassignMessageBlock(selectedNumbers)
+      setIsDeassignModalOpen(true)
+    } else {
+      showErrorNotification(t('no_numbers_selected_to_deassign'))
+    }
+  }
+
+  const handleCloseDeassignModal = () => {
+    getPhoneNumbers(
+      match.customerId,
+      match.groupId,
+      page,
+      rowsPerPage,
+      filterValues,
+      orderBy,
+      order,
+      debouncedNumberLike
+    )
+  }
+
+  const handleDeassign = () => {
+    const payload = {
+      customerId: match.customerId,
+      groupId: match.groupId,
+      numbers: numbers.filter(el => el.checked),
+      closeModal: () => setIsDeassignModalOpen(false),
+      callback: handleCloseDeassignModal
+    }
+    deassignNumbersSubLvl(payload)
+  }
+
   const titleData = {
     mainText: t('phone_numbers')
   }
@@ -188,6 +310,35 @@ const PhoneNumbers = observer(({ t }) => {
           <Typography className={classes.addCustomerTitle}>
             {t('filters')}
           </Typography>
+        </Box>
+        <Box>
+          <Box className={`${classes.buttonContainer} ${classes.deassignWrap}`}>
+            <IconButton
+              aria-label='deassign icon button'
+              component='span'
+              className={classnames(classes.mainIconWrap, {
+                [classes.disabledButton]: !numbers
+                  .filter(el => el.status !== 'used')
+                  .some(el => el.checked)
+              })}
+              onClick={handleDeassignButtonClick}
+            >
+              <img
+                className={classes.deassignIcon}
+                src={deassignIcon}
+                alt='deassign from subaccount'
+              />
+            </IconButton>
+            <Typography
+              className={classnames(classes.iconTitle, {
+                [classes.disabledContent]: !numbers
+                  .filter(el => el.status !== 'used')
+                  .some(el => el.checked)
+              })}
+            >
+              {t('deassign_from_subaccount')}
+            </Typography>
+          </Box>
         </Box>
       </Box>
     )
@@ -239,22 +390,71 @@ const PhoneNumbers = observer(({ t }) => {
       getCellData: row => (
         <Fragment>{t(row.service_capabilities).toLowerCase()}</Fragment>
       )
+    },
+    {
+      id: 'subaccount',
+      label: '',
+      headIcon: (
+        <StyledTooltip title={t('deassign_all_tooltip')}>
+          <IconButton
+            aria-label='deassign icon button'
+            component='span'
+            className={classnames(classes.tableIconHeaderWrap, {
+              [classes.btnBack]: numbers
+                .filter(el => el.status !== 'used')
+                .every(el => el.checked).length,
+              [classes.enabledColumnButton]: numbers.every(
+                el => el.status !== 'used'
+              )
+            })}
+          >
+            <img
+              className={classes.deassignIcon}
+              src={deassignIcon}
+              alt='deassign'
+            />
+          </IconButton>
+        </StyledTooltip>
+      ),
+      onIconClick: handleDeassignAll,
+      getCellData: row => (
+        <Box>
+          <Fragment>
+            {isDeassigningNumber && row.checked ? (
+              <CircularProgress className={classes.deleteLoading} />
+            ) : (
+              <StyledTooltip
+                title={
+                  row.status === 'used'
+                    ? t('not_available_to_deassign_tooltip')
+                    : t('available_to_deassign_tooltip')
+                }
+              >
+                <IconButton
+                  aria-label='deassign icon button'
+                  component='span'
+                  className={classnames(classes.tableIconWrap, {
+                    [classes.btnBack]: row.checked,
+                    [classes.enabledColumnButton]: row.checked
+                  })}
+                  onClick={
+                    row.status === 'used'
+                      ? () => {}
+                      : () => singleDeassignSelect(row)
+                  }
+                >
+                  <img
+                    className={classes.deassignIcon}
+                    src={deassignIcon}
+                    alt='deassign from subaccount'
+                  />
+                </IconButton>
+              </StyledTooltip>
+            )}
+          </Fragment>
+        </Box>
+      )
     }
-    // {
-    //   id: 'delete',
-    //   extraProps: {
-    //     className: classes.deleteCell,
-    //     align: 'right'
-    //   },
-    //   isSortAvailable: false,
-    //   getCellData: row => (
-    //     <Fragment>
-    //       {row.status === 'free' && (
-    //         <CloseOutlinedIcon className={classes.deleteCustomerIcon} />
-    //       )}
-    //     </Fragment>
-    //   )
-    // }
   ]
 
   return (
@@ -299,6 +499,19 @@ const PhoneNumbers = observer(({ t }) => {
           />
         )}
       </Paper>
+      {isDeassignModalOpen && (
+        <DeleteModal
+          classes={classes}
+          open={isDeassignModalOpen}
+          handleClose={handleCloseDeassignModal}
+          handleDelete={handleDeassign}
+          deleteSubject={deassignSubject}
+          extraDeleteSubject={deassignMessage}
+          action={t('to_deassign')}
+          titleAction={t(`deassign`)}
+          extraMessageBlock={deassignMessageBlock}
+        />
+      )}
     </div>
   )
 })
